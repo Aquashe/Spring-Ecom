4fc90f5814d2040ea9a0a73cd8d58cdbc11ad968
Aquashe <thomasukuttybenny2709@gmail.com>
2025-01-16 06:48:55 +0530

    Project Spring ecom added

---
+++ SpringEcomEx/SpringEcomEx/src/main/java/com/thomas/SpringEcomEx/ProductController.java
@@ -0,0 +1,15 @@
+package com.thomas.SpringEcomEx;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class ProductController {
+
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
+
+}
